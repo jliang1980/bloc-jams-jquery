998c335bfdf8f133f8237216494a2f7c986360e8
Jing <jingliang0503@gmail.com>
2020-01-13 11:58:33 -0500

    assignment 17 redo

---
+++ scripts/helper.js
@@ -0,0 +1,9 @@
+class Helper {
+	playPauseAndUpdate(song){
+	player.playPause(song);
+	const time = player.currentlyPlaying.duration;
+	$('#time-control.total-time').text(time);
+	}
+}
+
+const helper = new Helper();
