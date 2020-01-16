b8aaf3061e6863133961ab07be8ec4c04a1c2217
Jing <jingliang0503@gmail.com>
2020-01-16 12:48:30 -0500

    new

--- scripts/song-list.js
+++ scripts/song-list.js
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-{
-
-album.songs.forEach( ( song, index) => {
-song.element = $(`
-<tr>
-	<td>
-	 <button>
-=======
  {
    album.songs.forEach( (song, index) => {
      song.element = $(`
@@ -14,26 +5,10 @@ song.element = $(`
        
        <td>
           <button>
->>>>>>> 3ab3d810195adada201f9bd4126ba61f773f30d1
             <span class="song-number">${index + 1}</span>
             <span class="ion-play"></span>
             <span class="ion-pause"></span>
           </button>
-<<<<<<< HEAD
-</td>
-	<td>${song.title}</td>
-    <td>${song.duration}</td>
-	</tr>
-`);
-	song.element.on('click', event => {
-		player.playPause(song);
-	});
-
-	
-	$ ('#song-list').append(song.element);
-});	
-}
-=======
         </td>
          <td>${song.title}</td>
          <td>${song.duration}</td>
@@ -47,4 +22,3 @@ song.element = $(`
      $('#song-list').append(song.element);    
    });
  }
->>>>>>> 3ab3d810195adada201f9bd4126ba61f773f30d1
